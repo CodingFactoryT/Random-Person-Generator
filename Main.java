219791d4f802de87d71f34bde1e1db8910fe69fc
CodingFactoryT <94389494+CodingFactoryT@users.noreply.github.com>
2023-02-15 22:22:25 +0100

    added ability to generate multiple persons at once

--- Main.java
+++ Main.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
 
 public class Main {
 	
 	public static void main(String[] args) {
-		Person person = new Person();
-		System.out.println(person);
-		System.out.println(person.getAsCSV());
+		System.out.println("How many persons would you like to generate?");
+		Scanner scanner = new Scanner(System.in);
+		while(!scanner.hasNextInt()){
+			System.out.println("Please input an integer value:");
+			scanner.nextLine();
+		}
+
+		int count = scanner.nextInt();
+		
+		for(int i = 0; i < count; i++){
+			Person person = new Person();
+			//System.out.println(person);
+			System.out.println(person.getAsCSV());
+		}
 	}
 }
